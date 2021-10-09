70f02785cbeef0e5a0c7e4e1e7a5a5fa54e94413
Daxter <daxter5onyx@gmail.com>
2021-10-09 20:36:53 +0200

    Fire

--- Sacrificial Dance/Assets/Scripts/DeplacementManager.cs
+++ Sacrificial Dance/Assets/Scripts/DeplacementManager.cs
@@ -2,38 +2,69 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DeplacementManager : MonoBehaviour
 {
+    internal static UnityEvent InFire = new UnityEvent();
+    internal static UnityEvent OutFire = new UnityEvent();
+    
     public float speed = 1;
-    private Camera camera;
+    private Camera _camera;
+    private Rigidbody2D _rb;
 
+    [Header("In Fire")] 
+    public float speedInFire = 0.5f;
+    bool inFire = false;
+        
     private void Start()
     {
-        camera = Camera.main;
+        _rb = GetComponent<Rigidbody2D>();
+        _camera = Camera.main;
+        InFire.AddListener(GoInFire);
+        OutFire.AddListener(GoOutFire);
     }
 
+    private void GoInFire()
+    {
+        inFire = true;
+    }
+
+    private void GoOutFire()
+    {
+        inFire = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        var mouse = camera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mouse = _camera.ScreenToWorldPoint(Input.mousePosition);
 
-        mouse.z = 0;
-
-        Vector3 diff = (mouse - transform.position);
-        if (diff.magnitude > speed)
+        Vector2 diff = (mouse - _rb.position);
+        float trueSpeed = inFire ? speedInFire : speed;
+        if (diff.magnitude > trueSpeed)
         {
-            transform.position += diff.normalized * speed;
+            _rb.MovePosition(_rb.position + diff.normalized * trueSpeed);
         }
         else
         {
-            transform.position = mouse;
+            _rb.MovePosition(mouse);
         }
     }
 
-    private void OnCollisionEnter(Collision other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("colide");
+        if (other.CompareTag("Fire"))
+        {
+            InFire?.Invoke();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Fire"))
+        {
+            OutFire?.Invoke();
+        }
     }
 }
