e44eef17d098e749901a5afeb048b84cde684bbe
Daxter <daxter5onyx@gmail.com>
2021-10-10 22:24:46 +0200

    Double speed

--- Sacrificial Dance/Assets/Scripts/Rotate.cs
+++ Sacrificial Dance/Assets/Scripts/Rotate.cs
@@ -1,16 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
     public float Speed = 1f;
+    public float SpeedPhase3 = 30f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,0,Speed * Time.deltaTime);
+        var speed = MusicManager.index == 3 ? SpeedPhase3 : Speed;
+        transform.Rotate(0,0,speed * Time.deltaTime);
         for (int i = 0; i < transform.childCount; i++)
         {
             transform.GetChild(i).transform.rotation = Quaternion.identity;
